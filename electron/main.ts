b6365a5c052f733abeb78bf071cc312078a775a6
Tyler Yih <tyler.yih985@gmail.com>
2026-01-17 22:10:14 -0800

    Cheers love

--- electron/main.ts
+++ electron/main.ts
@@ -1,91 +1,70 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
-import { exec } from 'child_process'
+import { app, BrowserWindow } from 'electron'
+import { createRequire } from 'node:module'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 
+const require = createRequire(import.meta.url)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-let mainWindow: BrowserWindow | null
+// The built directory structure
+//
+// ├─┬─┬ dist
+// │ │ └── index.html
+// │ │
+// │ ├─┬ dist-electron
+// │ │ ├── main.js
+// │ │ └── preload.mjs
+// │
+process.env.APP_ROOT = path.join(__dirname, '..')
 
-let repoPath: string = ""
+// 🚧 Use ['ENV_NAME'] avoid vite:define plugin - Vite@2.x
+export const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
+export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
+export const RENDERER_DIST = path.join(process.env.APP_ROOT, 'dist')
 
-declare const MAIN_WINDOW_WEBPACK_ENTRY: string
-declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, 'public') : RENDERER_DIST
 
+let win: BrowserWindow | null
 
-
-// const assetsPath =
-//   process.env.NODE_ENV === 'production'
-//     ? process.resourcesPath
-//     : app.getAppPath()
-
-function createWindow () {
-  mainWindow = new BrowserWindow({
-    // icon: path.join(assetsPath, 'assets', 'icon.png'),
-    width: 1100,
-    height: 700,
-    backgroundColor: '#191622',
+function createWindow() {
+  win = new BrowserWindow({
+    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
-    }
+      preload: path.join(__dirname, 'preload.mjs'),
+      nodeIntegration: true
+    },
+    frame: false,
   })
 
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
-
-  mainWindow.on('closed', () => {
-    mainWindow = null
+  // Test active push message to Renderer-process.
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', (new Date).toLocaleString())
   })
+
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL)
+  } else {
+    // win.loadFile('dist/index.html')
+    win.loadFile(path.join(RENDERER_DIST, 'index.html'))
+  }
 }
 
-
-async function registerListeners () {
-  /**
-   * This comes from bridge integration, check bridge.ts
-   */
-  ipcMain.on('message', (_, message) => {
-    console.log(message)
-  })
-
-  ipcMain.handle('git:run', async (_event, command: string) => {
-    return new Promise((resolve, reject) => {
-      // run git with cwd set to repoPath
-      exec(`git ${command}`, { cwd: repoPath }, (err, stdout, stderr) => {
-          console.log(`Running git ${command} in ${repoPath}`);
-          if (stdout) console.log('stdout:', stdout);
-          if (stderr) console.log('stderr:', stderr);
-
-          if (err) {
-            reject(stderr || err.message)
-          } else {
-            resolve(stdout)
-          }
-        }
-      );
-    });
-  });
-
-  ipcMain.handle('path:set', async (_event, path: string) => {
-    return new Promise((resolve) => {
-      repoPath = path
-      console.log(`repoPath set to ${repoPath}`)
-      resolve(repoPath)
-    });
-  });
-  
-}
-
-app.on('ready', createWindow)
-  .whenReady()
-  .then(registerListeners)
-  .catch(e => console.error(e))
-
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
+    win = null
   }
 })
 
 app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
 })
+
+app.whenReady().then(createWindow)
